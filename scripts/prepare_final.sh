73751677fae52a80bd51dc2dedf68b87d08fb6ee
TANAHASHI, Jiro (aka jtFuruhata) <jt@do-johodai.ac.jp>
2020-11-02 08:23:22 +0900

    add: spread and changeFps feature

--- scripts/prepare_final.sh
+++ scripts/prepare_final.sh
@@ -7,47 +7,107 @@
 # See LICENSE
 #
 
-datumDir="$1"
-cd "$datumDir"
-while read line; do
-    teamId=`echo $line | awk '{print $1}'`
-    combinedId=`echo $line | awk '{print $2}'`
-    if [ -d $teamId ]; then
-        mv $teamId $combinedId
-    fi
-done < "$ETROBO_ROOT/dist/teamlist.txt"
+# expand </path/to/datum>
+if [ "$1" == "expand" ]; then
+    datumDir="$2"
+    cd "$datumDir"
+    while read line; do
+        teamId=`echo $line | awk '{print $1}'`
+        combinedId=`echo $line | awk '{print $2}'`
+        if [ -d $teamId ]; then
+            mv $teamId $combinedId
+        fi
+    done < "$ETROBO_ROOT/dist/teamlist.txt"
 
-while read line; do
-    combinedId=`echo "$line" | awk '{print $1}'`
-    prefixies=`echo "$line" | awk '{print $2}'`
-    requestId=`echo "$line" | awk '{print $3}'`
-    course=${prefixies:0:2}
-    cd "$combinedId"
+    while read line; do
+        combinedId=`echo "$line" | awk '{print $1}'`
+        prefixies=`echo "$line" | awk '{print $2}'`
+        requestId=`echo "$line" | awk '{print $3}'`
+        course=${prefixies:0:2}
+        cd "$combinedId"
 
-    if [ ! -d l_race ] || [ ! -d r_race ]; then
-        echo "$line"
-        srcDir="$requestId/req"
-        srcName="`ls -1 $requestId/req 2>&1 | head -n 1`"
-        distName="${prefixies}${requestId}.zip"
-        cp "$srcDir/$srcName" "$distName" > /dev/null 2>&1
-        if [ "$?" != "0" ]; then
-            echo "cp error"
-            exit 1
-        else
-            unzip "$distName" > /dev/null 2>&1
+        if [ ! -d l_race ] || [ ! -d r_race ]; then
+            echo "$line"
+            srcDir="$requestId/req"
+            srcName="`ls -1 $requestId/req 2>&1 | head -n 1`"
+            distName="${prefixies}${requestId}.zip"
+            cp "$srcDir/$srcName" "$distName" > /dev/null 2>&1
             if [ "$?" != "0" ]; then
-                echo "unzip error"
+                echo "cp error"
                 exit 1
             else
-                if [ ! -f "__race/${course}__race.asp" ]; then
-                    echo "$combinedId ${course}__race.asp not found"
+                unzip "$distName" > /dev/null 2>&1
+                if [ "$?" != "0" ]; then
+                    echo "unzip error"
                     exit 1
                 else
-                    mv "__race" "${course}race"
+                    if [ ! -f "__race/${course}__race.asp" ]; then
+                        echo "$combinedId ${course}__race.asp not found"
+                        exit 1
+                    else
+                        mv "__race" "${course}race"
+                    fi
                 fi
             fi
         fi
-    fi
-    cd "$datumDir"
-done < "$ETROBO_ROOT/dist/requests.txt"
+        cd "$datumDir"
+    done < "$ETROBO_ROOT/dist/requests.txt"
 
+# spread </path/to/Results>
+elif [ "$1" == "spread" ]; then
+    results="$2"
+    cd "$results"
+    dist="`dirname $(pwd)`/up_`basename $(pwd)`"
+    rm -rf "$dist"
+    mkdir -p "$dist"
+    for line in `ls -1`; do
+        echo $line
+        requestID="`echo $line | sed -E 's/._._(.*)\.zip/\1/'`"
+        teamID="`cat \"$ETROBO_ROOT/dist/requests.txt\" | grep $requestID | awk '{print $1}'`"
+        course="`cat \"$ETROBO_ROOT/dist/requests.txt\" | grep $requestID | awk '{print $2}' | sed -E 's/^(.)_._/\U\1/'`"
+        folderName="${teamID}_${course}"
+        innerName="`unzip -l $line | sed '1,3d' | head -n 1 | awk '{print $4}' | sed -E 's/^(.*)\/$/\1/'`"
+        cp "$line" "$dist/"
+        cd "$dist"
+        unzip -o "$line" > /dev/null
+        mv "$innerName" "$folderName"
+        rm "$line"
+        cd "$results"
+    done
+
+# changeFps </path/to/up_sim> [reencode <divider>] fps
+elif [ "$1" == "changeFps" ]; then
+    src="$2"
+    unset reencode
+    if [ "$3" == "reencode" ]; then
+        reencode="reencode"
+        divider="$4"
+        shift 2
+    fi
+    fps="$3"
+    if [ -z "$fps" ]; then
+        fps="60"
+    fi
+    cd "$src"
+    for line in `ls -1 "$src"`; do
+        echo $line
+        folderName="$line"
+        cd "$folderName"
+        target="`ls -1 | grep \"^[LR][0-9]\\{8\\}-.*.mp4$\" | head -n 1`"
+        if [ -z "$reencode" ]; then
+            ffmpeg -y -i $target -c copy -f h264 $target.h264 -loglevel 8
+            ffmpeg -y -r $fps -i $target.h264 -c copy $target -loglevel 8
+            rm $target.h264
+        else
+            ffmpeg -y -i $target -vf "setpts=${divider}*PTS" -r $fps $target.reencode.mp4
+            rm $target
+            mv $target.reencode.mp4 $target
+        fi
+        cd "$src"
+    done
+else
+    echo "usage:"
+    echo "  prepare_final.sh expand </path/to/Datum>"
+    echo "  prepare_final.sh spread </path/to/Results>"
+    echo "  prepare_final.sh changeFps </path/to/up_sim> [reencode <divider>] fps"
+fi
